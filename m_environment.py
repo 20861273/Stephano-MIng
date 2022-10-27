188d2f3e91bb9180981cfda1f6f53e47d192f3ed
20861273 <53533196+20861273@users.noreply.github.com>
2022-10-27 11:33:43 +0200

    Add files via upload

---
+++ m_environment.py
@@ -0,0 +1,92 @@
+from enum import Enum
+from collections import namedtuple
+import numpy as np
+import random
+import math
+
+# Environment characteristics
+HEIGHT = 4
+WIDTH = 4
+# DENSITY = 30 # percentage
+
+# Direction states
+class Direction(Enum):
+    RIGHT = 0
+    LEFT = 1
+    UP = 2
+    DOWN = 3
+
+# Block states
+class States(Enum):
+    UNEXP = 0
+    OBS = 1
+    ROBOT = 2
+    GOAL = 3
+    EXP = 4
+
+# Setup position variable of robot as point
+Point = namedtuple('Point', 'x, y')
+
+
+class Environment:
+    def __init__(self, nr):
+        # Set robot(s) position
+        self.pos = [0]*nr
+        self.prev_pos = [0]*nr
+        self.starting_pos = [0]*nr
+        self.pos = self.starting_pos
+
+        # Generates grid (Grid[y,x])
+        self.grid = self.generate_grid(nr)
+
+        print("\nGrid size: ", self.grid.shape)
+
+    def generate_grid(self, nr):        
+        # Ggenerate grid of zeros 
+        grid = np.zeros((HEIGHT, WIDTH))
+
+        # Generate obstacles
+        # CODE GOES HERE
+
+        # Distance code to keep for now
+        # for i in range(0, nr):
+        #     self.starting_pos[i] = Point(indices[i,1], indices[i,0])
+        #     distance_to = 0
+        #     next_i = i
+        #     if not i == 0:
+        #         while distance_to < grid.shape[0]*0.7:
+        #             self.starting_pos[i] = Point(indices[next_i,1], indices[next_i,0])
+        #             distance_to = math.sqrt((self.starting_pos[i].x - self.starting_pos[i-1].x)**2 +
+        #                             (self.starting_pos[i].y - self.starting_pos[i-1].y)**2)
+        #             next_i += 1
+
+        #     grid[self.starting_pos[i].y, self.starting_pos[i].x] = States.ROBOT.value
+            
+        #     self.pos = self.starting_pos
+
+        # Set robot(s) start position
+        self.starting_pos = [0]*nr
+        indices = np.argwhere(grid == States.UNEXP.value)
+        np.random.shuffle(indices)
+        i = 0
+        self.starting_pos[0] = Point(indices[i,1], indices[i,0])
+        while self.starting_pos[0].y < self.grid.shape[0]/2:
+            self.starting_pos[0] = Point(indices[i,1], indices[i,0])
+            i += 1
+        while self.starting_pos[0].y >= self.grid.shape[0]/2:
+            self.starting_pos[1] = Point(indices[i,1], indices[i,0])
+            i += 1
+
+        for i in range(0, nr):
+            self.grid[self.starting_pos[i].y, self.starting_pos[i].x] = States.ROBOT.value
+            
+        self.pos = self.starting_pos
+
+        # Set goal position
+        indices = np.argwhere(grid == States.UNEXP.value)
+        np.random.shuffle(indices)
+        self.goal = Point(indices[0,1], indices[0,0])
+        
+        grid[self.goal.y, self.goal.x] = States.GOAL.value
+
+        return grid
---
+++ m_solver.py
@@ -0,0 +1,19 @@
+from m_qlearning import QLearning
+from m_environment import Environment
+
+method = input("With which method would you like to solve the SAR problem?\n1. Q-learning\n2. DQN\n3. A* algorithm\n4. Lawnmower pattern\n\nSelect method:")
+method_flag = int(method)
+
+if method_flag == 1:    # Q-learning
+    debug_q = input("\n1. Training mode\n2. Greedy policy\n3. Generated map\nSelect mode: ")
+    debug_flag = int(debug_q)
+    env = Environment(2)
+    ql = QLearning(env)
+    ql.run_qlearning(debug_flag)
+elif method_flag == 2:  # NN
+    pass
+elif method_flag == 3:  # A* algorithm
+    pass
+elif method_flag == 4: # Lawnmower pattern
+    pass
+    
